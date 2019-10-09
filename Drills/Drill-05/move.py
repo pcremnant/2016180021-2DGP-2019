0dad5799d732654d4718adb67610f0c626e06437
ParkChan <pcremnant@naver.com>
2019-10-09 16:14:07 +0900

    add Handle Events

---
+++ Drills/Drill-05/move.py
@@ -0,0 +1,24 @@
+from pico2d import *
+
+def handle_events():
+    global running
+    global dir
+    global nFaramNumber
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN:
+            if event.key == SDLK_RIGHT:
+                dir += 1
+            elif event.key == SDLK_LEFT:
+                dir -= 1
+            elif event.key == SDLK_ESCAPE:
+                running = False
+        elif event.type == SDL_KEYUP:
+            if event.key == SDLK_RIGHT:
+                dir -= 1
+            elif event.key == SDLK_LEFT:
+                dir += 1
+
+    pass
