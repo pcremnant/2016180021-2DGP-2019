7f5eee4a77652882f837943ff4da90cf013fcd0a
ParkChan <pcremnant@naver.com>
2019-10-09 16:17:50 +0900

    add image load

--- Drills/Drill-05/move.py
+++ Drills/Drill-05/move.py
@@ -3,7 +3,6 @@ from pico2d import *
 def handle_events():
     global running
     global dir
-    global nFaramNumber
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
@@ -22,3 +21,19 @@ def handle_events():
                 dir += 1
 
     pass
+
+
+running = True
+
+open_canvas()
+imgBackGround = load_image("KPU_GROUND.png")
+imgHandArrow = load_image("hand_arrow.png")
+imgCharacter = load_image("animation_sheet.png")
+
+while running:
+    clear_canvas()
+    update_canvas()
+    handle_events()
+    delay(0.01)
+
+close_canvas()
