309604d1224195e1ce78fd7093dd5012f3209998
ParkChan <pcremnant@naver.com>
2019-10-09 16:25:28 +0900

    add HandArrow Move

--- Drills/Drill-05/move.py
+++ Drills/Drill-05/move.py
@@ -1,24 +1,26 @@
 from pico2d import *
 
+nFrameMode = 0
+nMouseX = 0
+nMouseY = 0
+
 def handle_events():
     global running
-    global dir
+    global nFrameMode
+    global nMouseX
+    global nMouseY
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
             running = False
         elif event.type == SDL_KEYDOWN:
-            if event.key == SDLK_RIGHT:
-                dir += 1
-            elif event.key == SDLK_LEFT:
-                dir -= 1
-            elif event.key == SDLK_ESCAPE:
+            if event.key == SDLK_ESCAPE:
                 running = False
-        elif event.type == SDL_KEYUP:
-            if event.key == SDLK_RIGHT:
-                dir -= 1
-            elif event.key == SDLK_LEFT:
-                dir += 1
+        elif event.type == SDL_MOUSEMOTION:
+             nMouseX = event.x
+             nMouseY = event.y
+        #elif event.type == SDL_MOUSEBUTTONDOWN:
+# if event.key
 
     pass
 
@@ -32,6 +34,7 @@ imgCharacter = load_image("animation_sheet.png")
 
 while running:
     clear_canvas()
+    imgHandArrow.draw(nMouseX + 17, 600 - nMouseY - 23)
     update_canvas()
     handle_events()
     delay(0.01)
