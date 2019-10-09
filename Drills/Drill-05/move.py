f5467a9d030b4c01a3a760ede7d895f70d8830ca
ParkChan <pcremnant@naver.com>
2019-10-09 17:00:36 +0900

    add charater move add mouse handle

--- Drills/Drill-05/move.py
+++ Drills/Drill-05/move.py
@@ -8,6 +8,11 @@ nMouseX = 0
 nMouseY = 0
 nCharacterX = 100
 nCharacterY = 100
+nArriveX = 100
+nArriveY = 100
+
+bArrived = False
+bSetPoint = False
 
 
 def handle_events():
@@ -15,6 +20,8 @@ def handle_events():
     global nFrameMode
     global nMouseX
     global nMouseY
+    global nArriveX
+    global nArriveY
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
@@ -23,9 +30,11 @@ def handle_events():
             if event.key == SDLK_ESCAPE:
                 running = False
         elif event.type == SDL_MOUSEMOTION:
-            nMouseX = event.x
-            nMouseY = event.y
-        # elif event.type == SDL_MOUSEBUTTONDOWN:
+            nMouseX = event.x + 17
+            nMouseY = 600 - event.y - 23
+        elif event.type == SDL_MOUSEBUTTONDOWN:
+            nArriveX = event.x + 17
+            nArriveY = 600 - event.y - 23
     # if event.key
 
     pass
@@ -44,15 +53,53 @@ imgCharacter = load_image("animation_sheet.png")
 # frame 3 -> right wait
 # 800 x 400
 
+i = 0
+t = i / 100
+
+p1 = (nCharacterX, nCharacterY)
+p2 = (nArriveX, nArriveY)
+
 while running:
     clear_canvas()
     # imgBackGround.draw(100, 100)
+
     imgBackGround.clip_draw(0, 0, KPU_WIDTH, KPU_HEIGHT,
                             KPU_WIDTH / 2, KPU_HEIGHT / 2, 1280, 1024)
 
-    imgHandArrow.draw(nMouseX + 17, 600 - nMouseY - 23)
-    imgCharacter.clip_draw(nFrame * 100, nFrameMode * 100,
-                           100, 100, nCharacterX, nCharacterY)
+    imgHandArrow.draw(nMouseX, nMouseY)
+    if bArrived:
+        imgCharacter.clip_draw(nFrame * 100, nFrameMode * 100,
+                               100, 100, nCharacterX, nCharacterY)
+
+    if nCharacterX == nArriveX and nCharacterY == nArriveY:
+        bArrived = True
+    else:
+        bArrived = False
+
+    if bArrived:
+        if nFrameMode == 0:
+            nFrameMode = 2
+        elif nFrameMode == 1:
+            nFrameMode = 3
+    elif not bSetPoint:
+        bSetPoint = True
+        p1 = (nCharacterX, nCharacterY)
+        p2 = (nArriveX, nArriveY)
+    elif bSetPoint:
+        if i < 100:
+            i = i + 1
+            t = i / 100
+            x = (1 - t) * p1[0] + t * p2[0]
+            y = (1 - t) * p1[1] + t * p2[1]
+            imgCharacter.clip_draw(nFrame * 100, nFrameMode * 100,
+                                   100, 100, x, y)
+        elif i == 100 or t == 1:
+            nCharacterX = nArriveX
+            nCharacterY = nArriveY
+            i = 0
+            t = 0
+            bArrived = True
+            bSetPoint = False
     update_canvas()
     handle_events()
     nFrame = (nFrame + 1) % 8
