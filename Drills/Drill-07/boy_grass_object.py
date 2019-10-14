a873599e9ae20bb60c2bc76166c5017b7ef71bb5
ParkChan <pcremnant@naver.com>
2019-10-14 11:57:03 +0900

    add ball update()

--- Drills/Drill-07/boy_grass_object.py
+++ Drills/Drill-07/boy_grass_object.py
@@ -23,9 +23,6 @@ class Boy:
     def draw(self):
         self.image.clip_draw(self.frame * 100, 0, 100, 100, self.x, self.y)
 
-    def replace(self):
-        self.x = random.randint(100, 700)
-
 
 class Ball:
     def __init__(self, t):
@@ -33,13 +30,16 @@ class Ball:
         if t % 2 == 0:
             self.image = load_image("ball21x21.png")
         else:
-            self.image = load_image("ball42x42.png")
+            self.image = load_image("ball41x41.png")
 
     def update(self):
-        pass
+        if self.y>=30:
+            self.y -= 5
+        elif self.y<30:
+            self.y = 30
 
     def draw(self):
-        self.draw()
+        self.image.draw(self.x, self.y)
 
 
 def handle_events():
@@ -51,10 +51,6 @@ def handle_events():
             running = False
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
-        elif event.type == SDL_KEYDOWN and event.key == SDLK_r:
-            for boy in boys:
-                boy.replace()
-
 
 # initialization code
 open_canvas()
@@ -69,10 +65,15 @@ while running:
 
     for boy in boys:
         boy.update()
+    for ball in balls:
+        ball.update()
+
     clear_canvas()
     grass.draw()
     for boy in boys:
         boy.draw()
+    for ball in balls:
+        ball.draw()
 
     update_canvas()
 
