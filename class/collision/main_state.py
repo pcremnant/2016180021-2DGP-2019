dc69e6e4358973518e0b12c992445be975abdf1d
ParkChan <pcremnant@naver.com>
2019-11-14 15:04:34 +0900

    Lecture 14 complete

--- class/collision/main_state.py
+++ class/collision/main_state.py
@@ -8,7 +8,7 @@ import game_world
 
 from boy import Boy
 from grass import Grass
-from ball import Ball
+from ball import Ball, BigBall
 
 name = "MainState"
 
@@ -19,10 +19,15 @@ big_balls = []
 
 
 def collide(a, b):
+    left_a, bottom_a, right_a, top_a = a.get_bb()
+    left_b, bottom_b, right_b, top_b = b.get_bb()
+
+    if left_a > right_b: return False
+    if right_a < left_b: return False
+    if top_a < bottom_b: return False
+    if bottom_a > top_b: return False
     # fill here
     return True
-
-
 
 
 def enter():
@@ -34,14 +39,15 @@ def enter():
     grass = Grass()
     game_world.add_object(grass, 0)
 
+    global balls
+    balls = [Ball() for i in range(10)] + [BigBall() for i in range(10)]
+    game_world.add_objects(balls, 1)
     # fill here for balls
-
-
-
 
 
 def exit():
     game_world.clear()
+
 
 def pause():
     pass
@@ -57,7 +63,7 @@ def handle_events():
         if event.type == SDL_QUIT:
             game_framework.quit()
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
-                game_framework.quit()
+            game_framework.quit()
         else:
             boy.handle_event(event)
 
@@ -65,9 +71,17 @@ def handle_events():
 def update():
     for game_object in game_world.all_objects():
         game_object.update()
+    for ball in balls:
+        if collide(boy, ball):
+            balls.remove(ball)
+            game_world.remove_object(ball)
+            print("COLLISION")
+    for ball in balls:
+        if collide(grass, ball):
+            ball.stop()
 
+    delay(0.9)
     # fill here for collision check
-
 
 
 def draw():
@@ -75,9 +89,3 @@ def draw():
     for game_object in game_world.all_objects():
         game_object.draw()
     update_canvas()
-
-
-
-
-
-
